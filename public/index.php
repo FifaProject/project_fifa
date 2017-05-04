1032b94b4e18a25930bf24b5e581e4f6fcb2334a
demfi <fikret96@live.nl>
2017-05-04 09:27:13 +0200

    Begin website
    landingspage gemaakt

--- public/index.php
+++ public/index.php
@@ -2,9 +2,25 @@
 
     <div class="main-content">
 
-        <!-- site's main content goes here -->
-
-
+        <div class="pagetitle">
+            <div class="container">
+                <div class="logo">
+                   <img src="assets/img/logo3.png" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="banner">
+            <div class="container">
+                <img src="assets/img/banner-logo2.png" alt="">
+                <div class="loginscreen">
+                        <form action="login.php">
+                            <input type="text" id="username" placeholder="Username*">
+                            <input type="password" id="password" placeholder="Password*">
+                            <input class="loginbutton" type="button" value="Login">
+                        </form>
+                </div>
+            </div>
+        </div>
     </div>
 
 <?php require(realpath(__DIR__) . '/templates/footer.php');
