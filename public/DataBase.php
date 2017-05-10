3288ac005354c7f217b8829dd32f8bf5adfec2ed
demfi <fikret96@live.nl>
2017-05-10 15:09:27 +0200

    Addteams pagina update
    student id , voornaam , achternaam en poulenummer toegevoegd.

--- public/DataBase.php
+++ public/DataBase.php
@@ -1,3 +1,3 @@
 <?php
-$database = new PDO('mysql:host=localhost;dbname=website', 'root', '');
+$database = new PDO('mysql:host=localhost;dbname=project_fifa', 'root', '');
 $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
--- public/addteams.php
+++ public/addteams.php
@@ -21,15 +21,44 @@
                 <div class="addteams-leftcol">
                     <div class="leftcol-team">
                         <label  for="team">Teamnummer: </label><input type="text" id="team">
+                        <label for="poulenumber"> Poulenumber: </label><input type="number">
                     </div>
                 </div>
-                <div class="addteams-rightcol">
-                    <label for="speler">Speler 1 : </label><input type="text" id="speler">
-                    <label for="speler">Speler 2 : </label><input type="text" id="speler">
-                    <label for="speler">Speler 3 : </label><input type="text" id="speler">
-                    <label for="speler">Speler 4 : </label><input type="text" id="speler">
-                    <label for="speler">Speler 5 : </label><input type="text" id="speler">
-                    <label for="speler">Speler 6 : </label><input type="text" id="speler">
+                <div class="playercard">
+                    <h2>Speler 1</h2>
+                    <label for="speler1_id">Studentnummer : </label><input type="text" id="speler1_id">
+                    <label for="speler1_fname">Voornaam : </label><input type="text" id="speler1_fname">
+                    <label for="speler1_lname">Achternaam : </label><input type="text" id="speler1_lname">
+                </div>
+                <div class="playercard">
+                    <h2>Speler 2</h2>
+                    <label for="speler2_id">Studentnummer : </label><input type="text" id="speler2_id">
+                    <label for="speler2_fname">Voornaam : </label><input type="text" id="speler2_fname">
+                    <label for="speler2_lname">Achternaam : </label><input type="text" id="speler2_lname">
+                </div>
+                <div class="playercard">
+                    <h2>Speler 3</h2>
+                    <label for="speler3_id">Studentnummer : </label><input type="text" id="speler3_id">
+                    <label for="speler3_fname">Voornaam : </label><input type="text" id="speler3_fname">
+                    <label for="speler3_lname">Achternaam : </label><input type="text" id="speler3_lname">
+                </div>
+                <div class="playercard">
+                    <h2>Speler 4</h2>
+                    <label for="speler4_id">Studentnummer : </label><input type="text" id="speler4_id">
+                    <label for="speler4_fname">Voornaam : </label><input type="text" id="speler4_fname">
+                    <label for="speler4_lname">Achternaam : </label><input type="text" id="speler4_lname">
+                </div>
+                <div class="playercard">
+                    <h2>Speler 5</h2>
+                    <label for="speler5_id">Studentnummer : </label><input type="text" id="speler5_id">
+                    <label for="speler5_fname">Voornaam : </label><input type="text" id="speler5_fname">
+                    <label for="speler5_lname">Achternaam : </label><input type="text" id="speler5_lname">
+                </div>
+                <div class="playercard">
+                    <h2>Speler 6</h2>
+                    <label for="speler6_id">Studentnummer : </label><input type="text" id="speler6_id">
+                    <label for="speler6_fname">Voornaam : </label><input type="text" id="speler6_fname">
+                    <label for="speler6_lname">Achternaam : </label><input type="text" id="speler6_lname">
                 </div>
                 <input id="sendteams" type="submit" value="Registreer">
             </form>
