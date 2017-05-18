35615729cc450d52d2d9712410521be02e8607e2
demfi <fikret96@live.nl>
2017-05-18 11:37:36 +0200

    addpoule page
    design

---
+++ public/AddPoule.php
@@ -0,0 +1,24 @@
+<?php require (realpath(__DIR__) . '/templates/header.php'); ?>
+
+<div class="wrapper">
+    <div class="pagetitle">
+        <div class="container">
+            <img src="assets/img/logo2.png" alt="">
+        </div>
+    </div>
+    <div class="navbar">
+        <ul>
+            <li><a href="index.php">Homepagina</a></li>
+            <li><a href="#">Teams bekijken</a></li>
+            <li><a href="#">Gebruikers toevoegen</a></li>
+            <li><a href="addteams.php">Teams toevoegen</a></li>
+        </ul>
+    </div>
+    <div class="addpoule">
+        <div class="container">
+            <form action="addpoulehandler.php">
+                
+            </form>
+        </div>
+    </div>
+</div>
