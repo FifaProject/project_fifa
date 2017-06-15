b9f6cbed68a20c23dbbe54ffcb48ea1c602b786d
santeman <santidudok@hotmail.com>
2017-06-15 09:42:45 +0200

    prepare for login

--- app/matchhandler.php
+++ app/matchhandler.php
@@ -21,22 +21,17 @@ $inputError = "Foutieve invoer wordt niet geaccepteerd. Probeer het opnieuw.";
 $nothingFound = "Poule niet gevonden. Probeer het opnieuw";
 $succes = "Poule gevonden!";
 
-if ($_SERVER['REQUEST_METHOD'] == 'GET')
-{
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $pouleid = $_GET['pouleid'];
 
-    if (!empty($pouleid))
-    {
-        if (preg_match('/^[0-9]*$/', $pouleid))
-        {
-            if ($pouleid >= 1 && $pouleid <= 100)
-            {
+    if (!empty($pouleid)) {
+        if (preg_match('/^[0-9]*$/', $pouleid)) {
+            if ($pouleid >= 1 && $pouleid <= 100) {
                 $stmt = $database->prepare(("SELECT * FROM `tbl_teams` WHERE `poule_id` = :pouleid"));
                 $stmt->execute(array("pouleid" => $pouleid));
 
                 $result = $stmt->rowCount();
-                if ($result > 0)
-                {
+                if ($result > 0) {
 
 
                     header("Location: ../public/addpoule.php?succes=$succes&pouleid=$pouleid");
@@ -63,19 +58,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET')
 //                        echo '</ul>';
 //                    }
 
-                }else
-                    {
-                        header("Location: ../public/addpoule.php?message=$nothingFound");
-                    }
-            }else
-                {
-                    header("Location: ../public/addpoule.php?message=$inputError");
+                } else {
+                    header("Location: ../public/addpoule.php?message=$nothingFound");
                 }
-        }else
-            {
+            } else {
                 header("Location: ../public/addpoule.php?message=$inputError");
             }
-    } else
-        {
-            header("Location: ../public/addpoule.php?message=$empty");
+        } else {
+            header("Location: ../public/addpoule.php?message=$inputError");
         }
+    } else {
+        header("Location: ../public/addpoule.php?message=$empty");
+    }
+}
